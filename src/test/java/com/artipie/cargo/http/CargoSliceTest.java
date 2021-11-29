9203c1f90cb9fc9d37a29b0901c56255c53e196e
Alena <olena.gerasimova@gmail.com>
2021-11-29 13:24:16 +0300

    init: project setup (#3)
    Setup project and added dummy (for now) entry slice and tests.

---
+++ src/test/java/com/artipie/cargo/http/CargoSliceTest.java
@@ -0,0 +1,32 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/cargo-adapter/LICENSE
+ */
+package com.artipie.cargo.http;
+
+import com.artipie.http.hm.RsHasStatus;
+import com.artipie.http.hm.SliceHasResponse;
+import com.artipie.http.rq.RequestLine;
+import com.artipie.http.rq.RqMethod;
+import com.artipie.http.rs.RsStatus;
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for {@link CargoSlice}.
+ * @since 0.1
+ */
+class CargoSliceTest {
+
+    @Test
+    void returnsOk() {
+        MatcherAssert.assertThat(
+            new CargoSlice(),
+            new SliceHasResponse(
+                new RsHasStatus(RsStatus.OK),
+                new RequestLine(RqMethod.GET, "/")
+            )
+        );
+    }
+
+}
