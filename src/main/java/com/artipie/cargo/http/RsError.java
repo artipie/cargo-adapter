c37e6471ea9ceedf8a96b606f909f7993035ab70
olenagerasimova <olena.gereasimova@gmail.com>
2021-11-30 12:39:27 +0300

    Created `RsError` for error responses

---
+++ src/main/java/com/artipie/cargo/http/RsError.java
@@ -0,0 +1,51 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/cargo-adapter/LICENSE
+ */
+package com.artipie.cargo.http;
+
+import com.artipie.http.Response;
+import com.artipie.http.rs.common.RsJson;
+import com.google.common.collect.Lists;
+import java.util.Collection;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonStructure;
+
+/**
+ * Cargo error response.
+ * <a href="https://doc.rust-lang.org/cargo/reference/registries.html#web-api">Documentation</a>.
+ * @since 0.1
+ */
+public final class RsError extends Response.Wrap {
+
+    /**
+     * Ctor.
+     * @param messages Error messages
+     */
+    public RsError(final Collection<String> messages) {
+        super(new RsJson(RsError.format(messages)));
+    }
+
+    /**
+     * Ctor.
+     * @param messages Error messages
+     */
+    public RsError(final String... messages) {
+        this(Lists.newArrayList(messages));
+    }
+
+    /**
+     * Format proper json with provided error messages.
+     * @param messages Error messages
+     * @return Json structure
+     */
+    private static JsonStructure format(final Collection<String> messages) {
+        JsonArrayBuilder arr = Json.createArrayBuilder();
+        for (final String msg : messages) {
+            arr = arr.add(Json.createObjectBuilder().add("detail", msg).build());
+        }
+        return Json.createObjectBuilder().add("errors", arr).build();
+    }
+
+}
---
+++ src/test/java/com/artipie/cargo/http/RsErrorTest.java
@@ -0,0 +1,41 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/cargo-adapter/LICENSE
+ */
+package com.artipie.cargo.http;
+
+import com.artipie.http.headers.ContentLength;
+import com.artipie.http.headers.ContentType;
+import com.artipie.http.hm.RsHasBody;
+import com.artipie.http.hm.RsHasHeaders;
+import com.artipie.http.hm.RsHasStatus;
+import com.artipie.http.rs.RsStatus;
+import java.nio.charset.StandardCharsets;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for {@link RsError}.
+ * @since 0.1
+ * @checkstyle MagicNumberCheck (500 lines)
+ */
+class RsErrorTest {
+
+    @Test
+    void responseWithOkStatusJsonBodyAndHeader() {
+        MatcherAssert.assertThat(
+            new RsError("first message", "second message"),
+            Matchers.allOf(
+                new RsHasStatus(RsStatus.OK),
+                new RsHasHeaders(
+                    new ContentType("application/json; charset=UTF-8"),
+                    new ContentLength(67)
+                ),
+                // @checkstyle LineLengthCheck (1 line)
+                new RsHasBody("{\"errors\":[{\"detail\":\"first message\"},{\"detail\":\"second message\"}]}".getBytes(StandardCharsets.UTF_8))
+            )
+        );
+    }
+
+}
