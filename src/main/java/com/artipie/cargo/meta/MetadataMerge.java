e25bec858970ce22901db32341c0898dd6005310
Alena <olena.gerasimova@gmail.com>
2021-12-16 13:49:24 +0300

    Added class to merge cargo package metadata (#13)
    Co-authored-by: olenagerasimova <olena.gereasimova@gmail.com>

---
+++ src/main/java/com/artipie/cargo/meta/MetadataMerge.java
@@ -0,0 +1,56 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/cargo-adapter/LICENSE
+ */
+package com.artipie.cargo.meta;
+
+import com.artipie.asto.ArtipieIOException;
+import com.artipie.asto.misc.UncheckedIOConsumer;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+
+/**
+ * Merge new package metadata into optionally existing metadata file.
+ * @since 0.1
+ */
+public final class MetadataMerge implements BiConsumer<Optional<InputStream>, OutputStream> {
+
+    /**
+     * String metadata to add.
+     */
+    private final String add;
+
+    /**
+     * Ctor.
+     * @param add String metadata to add
+     */
+    public MetadataMerge(final String add) {
+        this.add = add;
+    }
+
+    @Override
+    @SuppressWarnings("PMD.AssignmentInOperand")
+    public void accept(final Optional<InputStream> input, final OutputStream output) {
+        try {
+            if (input.isPresent()) {
+                // @checkstyle MagicNumberCheck (1 line)
+                final byte[] buffer = new byte[4 * 1024];
+                int len;
+                while ((len = input.get().read(buffer)) > 0) {
+                    output.write(buffer, 0, len);
+                }
+                output.write("\n".getBytes(StandardCharsets.UTF_8));
+            }
+            output.write(this.add.getBytes(StandardCharsets.UTF_8));
+        } catch (final IOException err) {
+            throw new ArtipieIOException(err);
+        } finally {
+            Optional.of(output).ifPresent(new UncheckedIOConsumer<>(OutputStream::close));
+            input.ifPresent(new UncheckedIOConsumer<>(InputStream::close));
+        }
+    }
+}
---
+++ src/test/java/com/artipie/cargo/meta/MetadataMergeTest.java
@@ -0,0 +1,60 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/cargo-adapter/LICENSE
+ */
+package com.artipie.cargo.meta;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test for {@link MetadataMerge}.
+ * @since 0.1
+ */
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
+class MetadataMergeTest {
+
+    @Test
+    void addsWhenInputIsAbsent() {
+        final ByteArrayOutputStream res = new ByteArrayOutputStream();
+        final String line = "new package metadata line";
+        new MetadataMerge(line).accept(Optional.empty(), res);
+        MatcherAssert.assertThat(
+            res.toString(),
+            new IsEqual<>(line)
+        );
+    }
+
+    @Test
+    void appendsLine() {
+        final ByteArrayOutputStream res = new ByteArrayOutputStream();
+        final ByteArrayInputStream input = new ByteArrayInputStream(
+            String.join(
+                "\n",
+                "package version 0.1 metadata",
+                "package version 0.1.3 metadata",
+                "package version 0.2 metadata"
+            ).getBytes(StandardCharsets.UTF_8)
+        );
+        final String line = "new package version metadata line";
+        new MetadataMerge(line).accept(Optional.of(input), res);
+        MatcherAssert.assertThat(
+            res.toString(),
+            new IsEqual<>(
+                String.join(
+                    "\n",
+                    "package version 0.1 metadata",
+                    "package version 0.1.3 metadata",
+                    "package version 0.2 metadata",
+                    "new package version metadata line"
+                )
+            )
+        );
+    }
+
+}
