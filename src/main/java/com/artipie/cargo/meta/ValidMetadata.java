8c2d896e75961091479ea86424a5b369ed0a04a8
Alena <olena.gerasimova@gmail.com>
2021-12-17 16:32:38 +0300

    Added class to validate package name and version (#14)
    Co-authored-by: olenagerasimova <olena.gereasimova@gmail.com>

---
+++ src/main/java/com/artipie/cargo/meta/ValidMetadata.java
@@ -0,0 +1,82 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/cargo-adapter/LICENSE
+ */
+package com.artipie.cargo.meta;
+
+import com.artipie.ArtipieException;
+import java.io.StringReader;
+import java.util.regex.Pattern;
+import javax.json.Json;
+import javax.json.JsonObject;
+
+/**
+ * Valid package metadata.
+ * @since 0.1
+ */
+final class ValidMetadata {
+
+    /**
+     * Package name regex.
+     */
+    private static final Pattern NAME = Pattern.compile("[\\w-_]+");
+
+    /**
+     * SemVer 2 regex to check the version, see<br/>
+     * https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string.
+     * @checkstyle LineLengthCheck (5 lines)
+     */
+    private static final Pattern VER = Pattern.compile("^(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\\+([0-9a-zA-Z-]+(?:\\.[0-9a-zA-Z-]+)*))?$");
+
+    /**
+     * Cargo package metadata.
+     */
+    private final JsonObject meta;
+
+    /**
+     * Ctor.
+     * @param meta Cargo package metadata
+     */
+    ValidMetadata(final JsonObject meta) {
+        this.meta = meta;
+    }
+
+    /**
+     * Ctor.
+     * @param meta Cargo package metadata string
+     */
+    ValidMetadata(final String meta) {
+        this(Json.createParser(new StringReader(meta)).getObject());
+    }
+
+    /**
+     * Validate and return package name.
+     * @return Name
+     * @throws ArtipieException If name is not valid
+     */
+    String validName() {
+        final String name = this.meta.getString("name");
+        if (ValidMetadata.NAME.matcher(name).matches()) {
+            return name;
+        }
+        throw new ArtipieException(
+            "Package name is illegal, only alphanumerics chars and _ - are allowed"
+        );
+    }
+
+    /**
+     * Validate and return package version. This must be a valid version number
+     * according to the Semantic Versioning 2.0.0.
+     * @return Version
+     * @throws ArtipieException If name is not valid
+     */
+    String validVersion() {
+        final String vers = this.meta.getString("vers");
+        if (ValidMetadata.VER.matcher(vers).matches()) {
+            return vers;
+        }
+        throw new ArtipieException(
+            "Package version is illegal,it should correspond to semver 2.0.0"
+        );
+    }
+}
---
+++ src/test/java/com/artipie/cargo/meta/ValidMetadataTest.java
@@ -0,0 +1,60 @@
+/*
+ * The MIT License (MIT) Copyright (c) 2020-2021 artipie.com
+ * https://github.com/artipie/cargo-adapter/LICENSE
+ */
+package com.artipie.cargo.meta;
+
+import com.artipie.ArtipieException;
+import javax.json.Json;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+/**
+ * Test for {@link ValidMetadata}.
+ * @since 0.1
+ */
+@SuppressWarnings("PMD.AvoidDuplicateLiterals")
+class ValidMetadataTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "a1", "a-b_c", "z_2-uy"})
+    void returnsName(final String name) {
+        MatcherAssert.assertThat(
+            new ValidMetadata(Json.createObjectBuilder().add("name", name).build()).validName(),
+            new IsEqual<>(name)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a/", "a(sdsd)="})
+    void validatesName(final String name) {
+        Assertions.assertThrows(
+            ArtipieException.class,
+            () -> new ValidMetadata(Json.createObjectBuilder().add("name", name).build())
+                .validName()
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1.0.0", "0.1.0", "2.0.0-alpha.1+001", "1.0.0-alpha.1"})
+    void returnsVersion(final String ver) {
+        MatcherAssert.assertThat(
+            new ValidMetadata(Json.createObjectBuilder().add("vers", ver).build()).validVersion(),
+            new IsEqual<>(ver)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"0.0", "xyz"})
+    void validatesVersion(final String ver) {
+        Assertions.assertThrows(
+            ArtipieException.class,
+            () -> new ValidMetadata(Json.createObjectBuilder().add("vers", ver).build())
+                .validVersion()
+        );
+    }
+
+}
